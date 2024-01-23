79f81b27891db7cdbb582152c00acefa22152af6
Tage Johansson <tajo9185@student.uu.se>
2024-01-23 16:13:15 +0100

    Improve base.py and add utils.py.

--- chess_cli/base.py
+++ chess_cli/base.py
@@ -1,15 +1,20 @@
+from .utils import *
+
 from dataclasses import dataclass, field
 from typing import *
 import os
 
 import appdirs  # type: ignore
+import chess
+import chess.pgn
 import cmd2
 import toml  # type: ignore
 
 
 @dataclass
 class InitArgs:
-    file_name: Optional[str] = None
+    "Arguments to the __init__() method of most ChessCli-classes."
+    pgn_file: Optional[str] = None
     config_file: str = field(
         default_factory=lambda: os.path.join(
             appdirs.user_config_dir("chess-cli"), "config.toml"
@@ -17,7 +22,37 @@ class InitArgs:
     )
 
 
+@dataclass
+class _GameHandle:
+    "The headers of a game together with either the offset of the game in a file or a node in the parsed game."
+    headers: chess.pgn.Headers
+    offset_or_game: int | chess.pgn.GameNode
+
+    @property
+    def offset(self) -> Optional[int]:
+        if isinstance(self.offset_or_game, int):
+            return self.offset_or_game
+        return None
+
+    @property
+    def game_node(self) -> Optional[chess.pgn.GameNode]:
+        if isinstance(self.offset_or_game, chess.pgn.GameNode):
+            return self.offset_or_game
+        return None
+
+
+class CommandFailure(cmd2.exceptions.SkipPostcommandHooks):
+    "An exception raised when a command fails and doesn't perform any updates to the game."
+    pass
+
+
 class Base(cmd2.Cmd):
+    _config_file: str  # The path to the currently open config file.
+    config: dict  # The current configuration as a dictionary.
+    _games: list[_GameHandle]  # A list of all currentlyopen games.
+    _pgn_file: Optional[TextIO]  # The currently open PGN file.
+    _game_idx: int  # The index of the currently selected game.
+
     def __init__(self, args: InitArgs):
         ## Initialize cmd2:
         shortcuts: dict[str, str] = dict(cmd2.DEFAULT_SHORTCUTS)
@@ -26,18 +61,93 @@ class Base(cmd2.Cmd):
 
         self.load_config(args.config_file)
 
+        ## Read the PGN file or initialize a new game:
+        self._games = []
+        self._pgn_file = None
+        self._game_idx = 0
+        if args.pgn_file is not None:
+            self.load_games(args.pgn_file)
+        else:
+            self.add_new_game()
+
     def load_config(self, config_file: str) -> None:
-        self.config_file: str = config_file
+        self._config_file = config_file
         try:
-            with open(self.config_file) as f:
-                self.config: dict = toml.load(f)
+            with open(self._config_file) as f:
+                self.config = toml.load(f)
                 if not isinstance(self.config, dict):
                     raise Exception("Failed to parse configuration")
         except Exception as ex:
             self.config = {}
             self.poutput(
-                f"Error while processing config file at '{self.config_file}': {repr(ex)}"
+                f"Error while processing config file at '{self._config_file}': {repr(ex)}"
             )
-            self.poutput(
-                "This session will be started with an empty configuration."
-            )
+            self.poutput("This session will be started with an empty configuration.")
+
+    def load_games(self, file_name: str) -> None:
+        "Load games from a PGN file. Upon success, all previous games will be discarded."
+        try:
+            with open(file_name) as pgn_file:
+                games: list[_GameHandle] = []
+                while True:
+                    offset: int = pgn_file.tell()
+                    headers = chess.pgn.read_headers(pgn_file)
+                    if headers is None:
+                        break
+                    games.append(_GameHandle(headers, offset))
+                if not games:
+                    self.poutput(f"Error: Couldn't find any game in {file_name}")
+                    raise CommandFailure()
+                # Reset analysis.
+                # TODO:
+                # self.stop_engines()
+                # self.init_analysis()
+                self._games = games
+                self._pgn_file = pgn_file
+                self.select_game(0)
+                self.poutput(f"Successfully loaded {len(self._games)} game(s).")
+        except OSError as ex:
+            self.poutput(f"Error: Loading of {file_name} failed: {ex}")
+
+    def add_new_game(self, idx: Optional[int] = None) -> None:
+        "Add a new game in the game list. If idx is None, append to the end of the game list."
+        game: chess.pgn.Game = chess.pgn.Game()
+        game_handle: _GameHandle = _GameHandle(game.headers, game)
+        if idx is None:
+            self._games.append(game_handle)
+            self._game_idx = len(self._games) - 1
+        else:
+            self._games.insert(idx, game_handle)
+            self._game_idx = idx
+
+    def select_game(self, idx: int) -> None:
+        "Select a game from the game list."
+        assert 0 <= idx < len(self._games)
+        game_handle = self._games[idx]
+        if game_handle.offset is not None:
+            assert self._pgn_file is not None
+            self._pgn_file.seek(game_handle.offset)
+            game_node = chess.pgn.read_game(self._pgn_file)
+            assert game_node is not None
+            self._games[idx] = _GameHandle(game_node.headers, game_node)
+        assert self._games[idx].game_node is not None
+        self._game_idx = idx
+
+    @property
+    def game_node(self) -> chess.pgn.GameNode:
+        "Get the currently selected position / game node."
+        x = self._games[self._game_idx].game_node
+        assert x is not None
+        return x
+
+    @game_node.setter
+    def game_node(self, val: chess.pgn.GameNode) -> None:
+        "Change the current position / game node."
+        self._games[self._game_idx] = _GameHandle(val.game().headers, val)
+
+    def set_prompt(
+        self, postcommand_data: cmd2.plugin.PostcommandData
+    ) -> cmd2.plugin.PostcommandData:
+        # Overrides method from Cmd2.
+        self.prompt = f"{move_str(self.game_node)}: "
+        return postcommand_data
---
+++ chess_cli/utils.py
@@ -0,0 +1,160 @@
+from . import nags
+
+import re
+from typing import *
+
+import chess.pgn
+
+
+def sizeof_fmt(num, suffix="B"):
+    "Print byte size with correct prefix."
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
+COMMANDS_IN_COMMENTS_REGEX: re.Pattern[str] = re.compile("\[%.+?\]")
+
+
+def commands_in_comment(raw_comment: str) -> str:
+    "Get a string with all embedded commands in a pgn comment."
+    return " ".join(COMMANDS_IN_COMMENTS_REGEX.findall(raw_comment))
+
+
+def comment_text(raw_comment: str) -> str:
+    """Strip out all commands like [%cal xxx] or [%clk xxx] from a comment."""
+    return " ".join(COMMANDS_IN_COMMENTS_REGEX.split(raw_comment)).strip()
+
+
+def update_comment_text(original_comment: str, new_text: str) -> str:
+    "Return a new comment with the same embedded commands but with the text replaced."
+    return f"{commands_in_comment(original_comment)}\n{new_text}"
+
+
+MOVE_NUMBER_REGEX: re.Pattern[str] = re.compile("(\d+)((\.{3})|\.?)")
+
+
+class MoveNumber(NamedTuple):
+    """A move number is a fullmove number and the color that made the move.
+    E.G. "1." would be move number 1 and color white while "10..." would be move number 10 and color black.
+    """
+
+    move_number: int
+    color: chess.Color
+
+    @staticmethod
+    def last(pos: Union[chess.Board, chess.pgn.ChildNode]):
+        """Get the move number from the previously executed move."""
+        if isinstance(pos, chess.pgn.ChildNode):
+            board = pos.board()
+        else:
+            board = pos
+        return MoveNumber(board.fullmove_number, board.turn).previous()
+
+    @staticmethod
+    def from_regex_match(match: re.Match):
+        "Create a move number from a regex match."
+        number: int = int(match.group(1))
+        if match.group(3) is not None:
+            color = chess.BLACK
+        else:
+            color = chess.WHITE
+        return MoveNumber(number, color)
+
+    @staticmethod
+    def parse(move_text: str):
+        """Parse a chess move number like "3." or "5...".
+        Plain numbers without any dots at the end will be parsed as if it was white who moved.
+        Will raise ValueError if the parsing failes.
+        """
+        match = MOVE_NUMBER_REGEX.fullmatch(move_text)
+        if match is None:
+            raise ValueError(f"Invalid move number {move_text}")
+        return MoveNumber.from_regex_match(match)
+
+    def previous(self):
+        "Get previous move."
+        if self.color == chess.WHITE:
+            return MoveNumber(self.move_number - 1, chess.BLACK)
+        else:
+            return MoveNumber(self.move_number, chess.WHITE)
+
+    def next(self):
+        "Get next move."
+        if self.color == chess.WHITE:
+            return MoveNumber(self.move_number, chess.BLACK)
+        else:
+            return MoveNumber(self.move_number + 1, chess.WHITE)
+
+    def __str__(self) -> str:
+        return str(self.move_number) + ("." if self.color == chess.WHITE else "...")
+
+    def __lt__(self, other) -> bool:
+        return (
+            self.move_number < other.move_number
+            or self.move_number == other.move_number
+            and self.color == chess.WHITE
+            and other.color == chess.BLACK
+        )
+
+    def __gt__(self, other) -> bool:
+        return (
+            self.move_number > other.move_number
+            or self.move_number == other.move_number
+            and self.color == chess.BLACK
+            and other.color == chess.WHITE
+        )
+
+    def __le__(self, other) -> bool:
+        return (
+            self.move_number < other.move_number
+            or self.move_number == other.move_number
+            and (self.color == chess.WHITE or other.color == chess.BLACK)
+        )
+
+    def __ge__(self, other) -> bool:
+        return (
+            self.move_number > other.move_number
+            or self.move_number == other.move_number
+            and (self.color == chess.BLACK or other.color == chess.WHITE)
+        )
+
+
+def move_str(
+    game_node: chess.pgn.GameNode,
+    include_move_number: bool = True,
+    include_sideline_arrows: bool = True,
+) -> str:
+    res: str = ""
+    if not isinstance(game_node, chess.pgn.ChildNode):
+        res += "start"
+    else:
+        if include_sideline_arrows and not game_node.is_main_variation():
+            res += "<"
+        if include_move_number:
+            res += str(MoveNumber.last(game_node)) + " "
+        if comment_text(game_node.starting_comment):
+            res += "-"
+        res += game_node.san()
+        if game_node.nags:
+            nag_strs = [nags.ascii_glyph(nag) for nag in game_node.nags]
+            if len(nag_strs) == 1:
+                res += nag_strs[0]
+            else:
+                res += f"[{', '.join(nag_strs)}]"
+    if (
+        comment_text(game_node.comment)
+        or game_node.arrows()
+        or game_node.eval() is not None
+        or game_node.clock() is not None
+    ):
+        res += "-"
+    if (
+        include_sideline_arrows
+        and game_node.parent is not None
+        and not game_node.parent.variations[-1] == game_node
+    ):
+        res += ">"
+    return res
