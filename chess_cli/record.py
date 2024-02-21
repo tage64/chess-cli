4ed76557221e48c1769164c2b7aed57e8548c2ec
Tage Johansson <tajo9185@student.uu.se>
2024-02-21 14:22:59 +0100

    "Add ffmpeg stderr handling."]

--- chess_cli/record.py
+++ chess_cli/record.py
@@ -1,6 +1,11 @@
 import asyncio
+import os
+import re
 import sys
+import tempfile
+import threading
 from asyncio import subprocess
+from contextlib import suppress
 from dataclasses import dataclass
 
 import pyaudio
@@ -10,6 +15,7 @@ from .base import Base, CommandFailure, InitArgs
 SAMPLE_FORMAT = pyaudio.paInt16
 SAMPLE_FORMAT_TO_FFMPEG = f"s16{"le" if sys.byteorder == "little" else "be"}"
 MAX_CHANNELS: int = 2
+ERROR_REGEX: re.Pattern = re.compile("error", flags=re.IGNORECASE)
 
 
 @dataclass
@@ -17,8 +23,14 @@ class Recording:
     """Data for an ongoing recording."""
 
     ffmpeg_process: subprocess.Process
+    ffmpeg_stderr_file: str  # Path to a temporary file holding the stderr from ffmpeg.
     audio_stream: pyaudio.Stream
-    shall_terminate: bool = False
+    terminate: threading.Event
+
+    def __del__(self) -> None:
+        with suppress(FileNotFoundError):
+            os.remove(self.ffmpeg_stderr_file)
+            print("removed!")
 
 
 class Record(Base):
@@ -39,8 +51,14 @@ class Record(Base):
         sample_rate: int = int(device_info["defaultSampleRate"])
         channels: int = min(device_info["maxInputChannels"], MAX_CHANNELS)  # type: ignore
         print(f"Connecting to {name} with {sample_rate} kHz and {channels} channels.")
+
+        ffmpeg_stderr_fd, ffmpeg_stderr_file = tempfile.mkstemp()
         ffmpeg_process = await asyncio.create_subprocess_exec(
             "ffmpeg",
+            "-hide_banner",
+            "-v",
+            "error",
+            "-n",
             "-f",
             SAMPLE_FORMAT_TO_FFMPEG,
             "-ar",
@@ -54,14 +72,14 @@ class Record(Base):
             "-b:a",
             "64k",
             "a.opus",
-            "-y",
             stdin=subprocess.PIPE,
             stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
+            stderr=ffmpeg_stderr_fd,
         )
 
         ffmpeg_stdin: asyncio.StreamWriter | None = ffmpeg_process.stdin
         assert ffmpeg_stdin is not None
+        terminate: threading.Event = threading.Event()
 
         def stream_callback(
             in_data: bytes | None, _frame_count: int, _time_info, _status_flags
@@ -76,8 +94,9 @@ class Record(Base):
                     action = pyaudio.paAbort
                     print(f"{type(e)}: {e}")
                 else:
-                    if self._curr_recording is not None and self._curr_recording.shall_terminate:
+                    if terminate.is_set():
                         action = pyaudio.paComplete
+                        ffmpeg_stdin.close()
                     else:
                         action = pyaudio.paContinue
             return (None, action)
@@ -90,12 +109,22 @@ class Record(Base):
             input_device_index=device_index,
             stream_callback=stream_callback,
         )
-        self._curr_recording = Recording(ffmpeg_process, audio_stream)
+        self._curr_recording = Recording(
+            ffmpeg_process, ffmpeg_stderr_file, audio_stream, terminate
+        )
 
     async def stop_recording(self) -> None:
         if self._curr_recording is None:
             raise CommandFailure("No recording is in progress.")
-        self._curr_recording.shall_terminate = True
-        while not self._curr_recording.audio_stream.is_stopped():
-            await asyncio.sleep(0.1)
-        self._curr_recording.audio_stream.close()
+        recording: Recording = self._curr_recording
+        recording.terminate.set()
+        await asyncio.sleep(0.1)
+        recording.audio_stream.close()
+        self._curr_recording = None
+        await recording.ffmpeg_process.wait()
+        with open(recording.ffmpeg_stderr_file) as f:
+            stderr: str = f.read()
+        if (retcode := recording.ffmpeg_process.returncode) != 0 or ERROR_REGEX.search(stderr):
+            self.perror(f"ffmpeg seem to have failed, return code: {retcode}")
+            self.perror(f"ffmpeg stderr: {stderr}")
+        del recording  # Make sure `recording.ffmpeg_stderr_file` is deleted.
