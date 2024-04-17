f6126f51e549e02e12825db2941629a5ebe2522f
Tage Johansson <tajo9185@student.uu.se>
2024-04-17 18:10:05 +0200

    Print a short prolog with version, author, license and help information at start.

--- chess_cli/__init__.py
+++ chess_cli/__init__.py
@@ -1,6 +1,12 @@
-# Add the dlls directory to path:
+import importlib.metadata
 import os
 
+__metadata__ = importlib.metadata.metadata("chess-cli")
+__version__ = __metadata__["version"]
+__author__ = __metadata__["author"]
+__package__ = "chess_cli"
+
+# Add the dlls directory to path:
 basepath = os.path.dirname(os.path.abspath(__file__))
 dllspath = os.path.join(basepath, "..", "dlls")
 os.environ["PATH"] = dllspath + os.pathsep + os.environ["PATH"]
--- chess_cli/main.py
+++ chess_cli/main.py
@@ -1,6 +1,9 @@
 import asyncio
 from argparse import ArgumentParser
 
+import spdx_license_list
+
+from . import __author__, __metadata__, __version__
 from .analysis_cmds import AnalysisCmds
 from .base import InitArgs
 from .curr_move_cmds import CurrMoveCmds
@@ -17,12 +20,25 @@ class Main(
     """Main class for the chess-cli app."""
 
     def __init__(self, args: InitArgs) -> None:
+        print(f"Welcome to Chess-CLI v{__version__}")
+        print(f"Author: {__author__} <{__metadata__["author-email"]}>")
+        print(f"Licensed under {spdx_license_list.LICENSES[__metadata__["license"]].name}")
+        print()
+        print("Type 'help' to get a list of possible commands.")
+        print()
         super().__init__(args)
 
 
 def main() -> None:
     """Run the program."""
     argparser = ArgumentParser(description="A repl to edit and analyse chess games.")
+    argparser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+        help="Show version and exit.",
+    )
     argparser.add_argument("pgn_file", nargs="?", help="Open the given pgn file.")
     argparser.add_argument("--config-file", type=str, help="Path to the config file.")
     args = argparser.parse_args()
