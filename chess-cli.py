81a5657257a988a177be1517047ba042d814a600
Tage Johansson <tajo9185@student.uu.se>
2024-07-31 19:16:13 +0200

    Make the Nuitka build work with typeguard.

--- chess-cli.py
+++ chess-cli.py
@@ -1,6 +1,13 @@
 # nuitka-project: --standalone
 # nuitka-project: --windows-console-mode=force
 # nuitka-project: --user-package-configuration-file={MAIN_DIRECTORY}/my.nuitka-package.config.yml
+
+if "__compiled__" in globals():
+    # Disable Typeguard as it tries to inspect the source code of functions
+    # which is not availlable when compiling with Nuitka.
+    import os
+
+    os.environ["TYPEGUARD_DISABLE"] = "1"
 import chess_cli.main
 
 if __name__ == "__main__":
